44a8dc7cf0b0d9efb842811a255ec8b790a0836f
Mike Wojtyna <mike@buildmysoftware.pro>
2019-11-05 13:46:53 +0100

    Remove monolithic certit project

--- certit/src/main/java/pro/buildmysoftware/certit/infrastructure/spring/persistence/CertificateRequestSpringDateRepository.java
+++
@@ -1,7 +0,0 @@
-package pro.buildmysoftware.certit.infrastructure.spring.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CertificateRequestSpringDateRepository extends CrudRepository<CertificateRequestData, String> {
-
-}
